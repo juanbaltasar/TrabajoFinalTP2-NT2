0d08397108cd2155cacde401d74982133c9b44ed
juanbaltasar <martinjuanbaltasar@gmail.com>
2020-10-22 21:39:32 -0300

    Correcciones formato archivo ENV

--- Back/src/Modules/EmailSender.js
+++ Back/src/Modules/EmailSender.js
@@ -7,8 +7,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.mail,
-        pass: process.env.pass
+        user: process.env.MAIL,
+        pass: process.env.PASS
     }
 });
 
@@ -16,21 +16,21 @@ function MandarMail(A, Asunto = 'Sin Asunto', Texto = '', Html = '') {
     let OpcionesMail
     if (Texto != '') {
         OpcionesMail = {
-            from: process.env.mail,
+            from: process.env.MAIL,
             to: A,
             subject: Asunto,
             text: Texto
         };
     } else {
         OpcionesMail = {
-            from: process.env.mail,
+            from: process.env.MAIL,
             to: A,
             subject: Asunto,
             html: Html
         };
     }
 
-    transporter.sendMail(OpcionesMail/*, (error, info) => {
+    transporter.sendMail(OpcionesMail, (error, info) => {
         if (error) {
             console.log(error);
             return error;
@@ -38,10 +38,9 @@ function MandarMail(A, Asunto = 'Sin Asunto', Texto = '', Html = '') {
             console.log(info);
             return info;
         }
-    }*/).then((ok) => ok).catch((error) => error)
+    })
 }
 
-//console.log(process.env.pass, process.env.mail)
 //MandarMail("martinjuanbaltasargmail.com", "Hola", "", "");
 
 exports.MandarMail = MandarMail;
