af3c7e18e191b39075556b2ea04b038f5ba8bc83
AGM <gonmaralv@gmail.com>
2020-10-29 01:55:16 -0300

    Modulo Download Funcionando

---
+++ Back/test/server.test.js
@@ -0,0 +1,17 @@
+// const {enviarReceta} = require('../src/Modules/downloadModulo.js')
+// const express = require('express')
+// const app = express()
+// const PORT = 1234
+
+// describe('si no encuentra el archivo', () => {
+//     describe('lanza un error', () => {
+//         enviarReceta('/')
+//     })
+// })
+
+// app.listen(PORT, function (err) {
+//     if (err) console.log(err);
+//     console.log("Server listening on PORT", PORT);
+// })
+
+// app.close()
