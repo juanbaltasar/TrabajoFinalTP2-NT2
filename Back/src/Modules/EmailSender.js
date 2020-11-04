139ea65e9951926ea43f7aa023832669a081cf52
juanbaltasar <martinjuanbaltasar@gmail.com>
2020-11-04 12:53:25 -0300

    Modulo Email finalizado

--- Back/src/Modules/EmailSender.js
+++ Back/src/Modules/EmailSender.js
@@ -1,53 +1,99 @@
-require('dotenv').config({
-    path: __dirname + '/../../config.env'
-});
-
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.MAIL,
-        pass: process.env.PASS
+class EnviadorDeMails{
+    constructor(configuracion){
+        this.configuracion = configuracion
+        this.transporter = nodemailer.createTransport(configuracion);
     }
-});
 
-async function MandarMail(A, Asunto = 'Sin Asunto', Texto = '', Html = '') {
-        let OpcionesMail
+    async MandarMail(A, Asunto = 'Sin Asunto', Texto = '', Html = '') { //El nyc me marca que esta linea no es cubierta por los tests, supongo que por los parametros default
+        let OpcionesMail;
         if (Texto != '') {
             OpcionesMail = {
-                from: process.env.MAIL,
+                from: this.configuracion.auth.user,
                 to: A,
                 subject: Asunto,
                 text: Texto
             };
         } else {
             OpcionesMail = {
-                from: process.env.MAIL,
+                from: this.configuracion.auth.user,
                 to: A,
                 subject: Asunto,
                 html: Html
             };
         }
-
+    
         let retorno;
-
-        retorno = await transporter.sendMail(OpcionesMail/*, (error, info) => {
-                if (error) {
-                    //console.log(error);
-                    //return error;
-                    retorno = error;
-                } else {
-                    //console.log(info);
-                    //return info;
-                    retorno = info;
-                }
-            }*/)/*.then((res) => {retorno = res})*/.catch((err) => err)
-
-            return retorno;
-            //console.log('no retorno')
-
-            //MandarMail("martinjuanbaltasargmail.com", "Hola", "", "");
+        //retorno = await this.transporter.sendMail(OpcionesMail).catch((err) => err); //Cambiar por try - catch
+        try {
+            retorno = await this.transporter.sendMail(OpcionesMail);
+        } catch (error) {
+            throw new Error('Error in sending the mail')
         }
+        return retorno;
+    }
+}
 
-        exports.MandarMail = MandarMail;
+exports.EnviadorDeMails = EnviadorDeMails
+
+
+// require('dotenv').config({
+//     path: __dirname + '/../../config.env'
+// });
+
+// const nodemailer = require('nodemailer');
+
+// //Terminar de desacoplar mediante una funcion constructora o una clase
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.MAIL,
+//         pass: process.env.PASS
+//     }
+// });
+
+// async function MandarMail(A, Asunto = 'Sin Asunto', Texto = '', Html = '') {
+//     let OpcionesMail
+//     if (Texto != '') {
+//         OpcionesMail = {
+//             from: process.env.MAIL,
+//             to: A,
+//             subject: Asunto,
+//             text: Texto
+//         };
+//     } else {
+//         OpcionesMail = {
+//             from: process.env.MAIL,
+//             to: A,
+//             subject: Asunto,
+//             html: Html
+//         };
+//     }
+
+//     let retorno;
+    
+//     try {
+//         retorno = await transporter.sendMail(OpcionesMail);
+//     } catch (error) {
+//         throw new Error('Error in sending the mail')
+//     }//.catch((err) => err.name) //cambiar por un try - catch
+//     return retorno;
+// }
+
+// exports.MandarMail = MandarMail;
+
+
+/*, (error, info) => {
+        if (error) {
+            //console.log(error);
+            //return error;
+            retorno = error;
+        } else {
+            //console.log(info);
+            //return info;
+            retorno = info;
+        }
+    }*/
+
+/*.then((res) => {retorno = res})*/
--- Back/test/Modules.test.js
+++ Back/test/Modules.test.js
@@ -1,26 +1,66 @@
+require('dotenv').config({
+    path: __dirname + '/../config.env'
+});
+
 const { doesNotMatch } = require('assert')
 const assert = require('assert')
 
-const {MandarMail} = require('../src/Modules/EmailSender.js')
+const {EnviadorDeMails} = require('../src/Modules/EmailSender.js')
 
 describe('Modulo enviador de mails', function() {
+    
+    const Enviador = new EnviadorDeMails({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL,
+            pass: process.env.PASS
+        }
+    })
 
     describe('Si el mail de destino es incorrecto', function()  {
         it('lanza un error', async function() {
+            
             const MailSinArroba = 'holagmail.com'
-            const res = await MandarMail(MailSinArroba, 'Hola', 'Hello')
+            try {
+                const res = await Enviador.MandarMail(MailSinArroba, 'Hola', 'Hello')
+            } catch (error) {
+                assert(true)
+            }
+            //console.log(res.name);
             /*.then((res) => {
                        assert.strictEqual(res.name, 'Error')
                    })*/
-            assert.strictEqual(res.name, 'Error')
+            
         })
     })
 
-    describe('Si los datos son correctos', function() {
+    describe('Si los datos son correctos, al enviar texto', function() {
         it('Envia el mail y devuelve la informacion', async function() {
             this.timeout(5000)
             const MailCorrecto = 'martinjuanbaltasar@gmail.com'
-            const res = await MandarMail(MailCorrecto, 'Hola', 'Hello')
+            let res
+            try {
+                res = await Enviador.MandarMail(MailCorrecto, 'Hola', 'Hello')
+            } catch (error) {
+
+            }
+            /*.then((res) => {
+                            assert.strictEqual(res.response.includes('OK'), true)
+                        })*/
+            assert.strictEqual(res.response.includes('OK'), true)
+        })
+    })
+
+    describe('Si los datos son correctos, al enviar html', function() {
+        it('Envia el mail y devuelve la informacion', async function() {
+            this.timeout(5000)
+            const MailCorrecto = 'martinjuanbaltasar@gmail.com'
+            let res
+            try {
+                res = await Enviador.MandarMail(MailCorrecto, 'Hola', '', '<p>Soy un parrafo</p>')
+            } catch (error) {
+                
+            }
             /*.then((res) => {
                             assert.strictEqual(res.response.includes('OK'), true)
                         })*/
